bb744983a316d6e6da0f4f0ec95bbe0572db1749
Maor <maorbz858@gmail.com>
2024-09-25 16:44:43 +0300

    Refactor SubmitButton to minimize rerenders

--- src/features/weather-form/components/submit-button/SubmitButton.tsx
+++ src/features/weather-form/components/submit-button/SubmitButton.tsx
@@ -1,17 +1,29 @@
 import '@/features/weather-form/components/submit-button/SubmitButton.css';
 
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '@/store';
-import { setWeatherFormValueAfterSubmit } from '@/features/weather-form/weatherFormSlice';
+import { useSelector } from 'react-redux';
+import { createSelector } from '@reduxjs/toolkit';
+import { RootState } from '@/store';
+
+const selectWeatherDataLoading = (state: RootState) => state.weatherData.isLoading;
+const selectGeolocationStatus = (state: RootState) => state.weatherForm.geolocationStatus;
+const selectSearchType = (state: RootState) => state.weatherForm.searchType;
+
+const selectSubmitButtonState = createSelector(
+    [selectWeatherDataLoading, selectGeolocationStatus, selectSearchType],
+    (isDataLoading, geolocationStatus, searchType) => ({
+        isDataLoading,
+        isGeolocationNotGranted: searchType === 'current-location' && geolocationStatus !== 'granted',
+    })
+);
 
 export function SubmitButton() {
-    const dispatch = useDispatch<AppDispatch>();
+    const { isDataLoading, isGeolocationNotGranted } = useSelector(selectSubmitButtonState);
 
     return (
         <button
             type="submit"
             className="weather-form__submit-button"
-            onClick={() => dispatch(setWeatherFormValueAfterSubmit())}
+            disabled={isDataLoading || isGeolocationNotGranted}
         >
             Get Weather
         </button>
